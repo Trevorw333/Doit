261c2f007c8d0a96fef698bf69dca68b04319ad1
trevor <welchtrevor333@live.com>
2017-05-07 20:35:01 -0700

    button text field and switch linked

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by trevor on 5/7/17.
+//  Copyright © 2017 trevor. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
