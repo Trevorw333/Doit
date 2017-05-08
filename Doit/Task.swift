297e67455613d0375dbec0a1aa16b05778ad94b5
trevor <welchtrevor333@live.com>
2017-05-07 19:39:36 -0700

    commit because it works right now

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by trevor on 5/7/17.
+//  Copyright © 2017 trevor. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
