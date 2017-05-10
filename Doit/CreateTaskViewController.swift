8c50c6fee8a82ad16ab05ad803ae1ffead33d82c
trevor <welchtrevor333@live.com>
2017-05-09 23:03:34 -0700

    in process of implementing core data but I need to go to bed 😴

--- Doit/CreateTaskViewController.swift
+++ Doit/CreateTaskViewController.swift
@@ -14,7 +14,6 @@ class CreateTaskViewController: UIViewController {
     
     @IBOutlet weak var importantSwitch: UISwitch!
     
-    var previousVC = ViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +24,16 @@ class CreateTaskViewController: UIViewController {
     @IBAction func addTapped(_ sender: Any) {
         //Create a task from the outlet information
         
-        let task = Task()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let task = Task(context: context)
         task.name = taskNameTextField.text!
         task.important = importantSwitch.isOn
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         
-        // Add new task to array in previous viewController
+        // pop back
         
-        previousVC.tasks.append(task)
-        previousVC.tableView.reloadData()
         navigationController!.popViewController(animated: true)
     }
     
--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tasks = makeTasks()
+        //tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -32,7 +32,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = UITableViewCell()
         let task = tasks[indexPath.row]
         if task.important {
-            cell.textLabel?.text = "⚠️ \(task.name)"
+            cell.textLabel?.text = "⚠️ \(task.name!)"
         } else {
             cell.textLabel?.text = task.name
         }
@@ -46,33 +46,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         performSegue(withIdentifier: "selectTaskSegue", sender: task)
     }
     
-    func makeTasks() -> [Task] {
-        let task1 = Task()
-        task1.name = "Rescue dog"
-        task1.important = true
-        
-        let task2 = Task()
-        task2.name = "Buy dog food"
-        task2.important = true
-        
-        let task3 = Task()
-        task3.name = "order corn"
-        task3.important = false
-        
-        return [task1, task2, task3]
-    }
     
     
     @IBAction func plusTapped(_ sender: Any) {
         performSegue(withIdentifier: "addSegue", sender: nil)
     }
     
+    func getTasks() {
+        
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "addSegue" {
-            
-        let nextVC = segue.destination as! CreateTaskViewController
-        nextVC.previousVC = self
-        }
         if segue.identifier == "selectTaskSegue" {
             let nextVC = segue.destination as! CompleteTaskViewController
             nextVC.task = sender as! Task
